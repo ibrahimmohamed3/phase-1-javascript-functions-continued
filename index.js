aaad5319a1ca51c296883a5880bf2df20ebcc621
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-15 20:54:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(){
+    console.log("roller-skate")
+}
+saturdayFun()
+function mondayWork(){
+    console.log("go to the office")
+}
+mondayWork()
+function wrapAdjective(){
+    console.log()
+}
